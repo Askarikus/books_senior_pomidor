192c59c062467c7a201903a7eee1d474eaf6328f
Askarikus <askarikus@yandex.ru>
2022-02-06 20:55:54 +0500

    video # 4 Oauth and video #5 create(post), update(put), delete and test for them

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -8,6 +11,7 @@ from store.serializers import BooksSerializer
 
 class BookApiTestCase(APITestCase):
     def setUp(self) -> None:
+        self.user = User.objects.create(username='test_user')
         self.book1 = Book.objects.create(name='Hotel David Linch', price=77.33,
                                          author_name='Arthur Haighley')
         self.book2 = Book.objects.create(name='Airport', price=88.50,
@@ -42,3 +46,49 @@ class BookApiTestCase(APITestCase):
         serializer_data = BooksSerializer([self.book3, self.book2, self.book1], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+        data = {
+            "name": "Sweet Chili Tyrells",
+            "price": 30,
+            "author_name": "Some British Author"
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data,
+                                    content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book1.id,))
+        data = {
+            "name": self.book1.name,
+            "price": 300,
+            "author_name": self.book1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url, data=json_data,
+                                   content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book1 = Book.objects.get(id=self.book1.id)
+        self.book1.refresh_from_db()
+        self.assertEqual(300, self.book1.price)
+
+    def test_delete(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-detail', args=(self.book1.id,))
+        data = {
+            "name": self.book1.name,
+            "price": self.book1.price,
+            "author_name": self.book1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.delete(url, data=json_data,
+                                      content_type='application/json')
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertEqual(2, Book.objects.all().count())
--- books/store/tests/test_serializers.py
+++ books/store/tests/test_serializers.py
@@ -6,19 +6,23 @@ from store.serializers import BooksSerializer
 
 class BooksSerializerTestCase(TestCase):
     def test_serializer_ok(self):
-        book1 = Book.objects.create(name='Hotel', price=77.33)
-        book2 = Book.objects.create(name='Airport', price=88.50)
+        book1 = Book.objects.create(name='Hotel', price=77.33,
+                                    author_name='Haighley')
+        book2 = Book.objects.create(name='Airport', price=88.50,
+                                    author_name='Haighley')
         data = BooksSerializer([book1, book2], many=True).data
         expected_data = [
             {
                 'id': book1.id,
                 'name': 'Hotel',
-                'price': '77.33'
+                'price': '77.33',
+                'author_name': 'Haighley',
              },
             {
                 'id': book2.id,
                 'name': 'Airport',
-                'price': '88.50'
+                'price': '88.50',
+                'author_name': 'Haighley'
             }
         ]
         self.assertEqual(expected_data, data)
